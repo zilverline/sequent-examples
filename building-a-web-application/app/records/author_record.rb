a4a9ff7ecde6ecc307a5c1029d78aa8514cf6549
Koen Werdler <kwerdler@zilverline.com>
2024-10-14 13:03:30 +0200

    Update example blog application from guides

--- building-a-web-application/app/records/author_record.rb
+++ building-a-web-application/app/records/author_record.rb
@@ -1,3 +1,3 @@
-class AuthorRecord < ActiveRecord::Base
-  has_many :post_records, foreign_key: :author_aggregate_id, primary_key: :aggregate_id
+class AuthorRecord < Sequent::ApplicationRecord
+  has_many :post_records, foreign_key: 'author_aggregate_id', primary_key: 'aggregate_id'
 end
